d1806b0ad5b3481b3f45bbed336c2391b3d9634c
Makendra <gamerchan756@gmail.com>
2026-03-17 12:15:47 -0400

    Initial Commit

---
+++ Food2Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food2Go
+//
+//  Created by Makendra Phipps on 3/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
