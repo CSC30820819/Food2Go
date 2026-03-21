ac0a4aae009e83001808b10bed882cf26cf2aaa3
Makendra <gamerchan756@gmail.com>
2026-03-21 19:41:26 -0400

    View Controllers and Scripts

---
+++ Food2Go/GrainCategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GrainCategoryViewController.swift
+//  Food2Go
+//
+//  Created by Makendra Phipps on 3/21/26.
+//
+
+import UIKit
+
+class GrainCategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
